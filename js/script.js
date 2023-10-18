608de30cc2697eae1bf936f59bb225f4c82ceead
chiara1911 <chiaracesari89@gmail.com>
2023-10-18 16:33:53 +0200

    append square

--- js/script.js
+++ js/script.js
@@ -4,12 +4,40 @@
 // 3. Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // >> Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
-// 1. creare tavola per campo minato X
+
+
+
 // 2. creare bottone per generare quadratini
-
-
 const btn = document.querySelector('button');
 
-btn.addEventListener('click', function(){
-    console.log(btn);
+// richiamo funzionalità al bottone
+btn.addEventListener('click', function () {
+    // dichiaro la costante della griglia intera
+    const playground = document.getElementById('playground');
+    // dichiaro quanti quadratini voglio in totale
+    const squareNum = 100;
+    //richiamo il ciclo per stampare i vari quadrati
+    for( let i = 0; i < squareNum; i++){
+
+    
+    // genero quadratino chiamando la funzione
+    let square = miniSquare(i);
+    console.log(square);
+
+    // appendo il quadratino alla griglia
+    playground.append(square);
+}
 });
+
+
+// fare una funzione per creare un quadratino
+
+function miniSquare(squareEl) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.innerHTML = squareEl + 1;
+    square.addEventListener('click', function(){
+        square.classList.add('active');
+    })
+    return square;
+}
