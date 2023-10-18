129be814a2de7c970063a338dd4d8ba3e9e81092
chiara1911 <chiaracesari89@gmail.com>
2023-10-18 15:38:49 +0200

    add btn event

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@
 // 2. Ogni cella ha un numero progressivo, da 1 a 100.
 // 3. Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // >> Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// 1. creare tavola per campo minato X
+// 2. creare bottone per generare quadratini
+
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function(){
+    console.log(btn);
+});
